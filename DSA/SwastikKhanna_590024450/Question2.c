7e041ed6acd9ddf4854ead5c04ced729595446b1
swastik7771 <swastik2007khanna@gmail.com>
2026-06-26 19:50:52 +0530

    Create Question2.c

---
+++ DSA/SwastikKhanna_590024450/Question2.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+
+int smallestDistinctWindow(char str[]) {
+    int n = strlen(str);
+
+    // Count total distinct characters
+    int visited[256] = {0};
+    int distinct = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (!visited[(unsigned char)str[i]]) {
+            visited[(unsigned char)str[i]] = 1;
+            distinct++;
+        }
+    }
+
+    // Sliding window
+    int freq[256] = {0};
+    int start = 0;
+    int count = 0;
+    int minLen = INT_MAX;
+
+    for (int end = 0; end < n; end++) {
+        freq[(unsigned char)str[end]]++;
+
+        if (freq[(unsigned char)str[end]] == 1)
+            count++;
+
+        // Window contains all distinct characters
+        while (count == distinct) {
+
+            // Update minimum length
+            if (end - start + 1 < minLen)
+                minLen = end - start + 1;
+
+            // Remove leftmost character
+            freq[(unsigned char)str[start]]--;
+
+            if (freq[(unsigned char)str[start]] == 0)
+                count--;
+
+            start++;
+        }
+    }
+
+    return minLen;
+}
+
+int main() {
+    char str[1000];
+
+    printf("Enter string: ");
+    scanf("%s", str);
+
+    printf("%d\n", smallestDistinctWindow(str));
+
+    return 0;
+}
